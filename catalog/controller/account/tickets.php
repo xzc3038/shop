5a37c938b208df245ebeab961ba74cd5fdc2cc7b
xzc <1805793040@qq.com>
2018-09-10 13:06:30 +0800

    update  13“06

---
+++ catalog/controller/account/tickets.php
@@ -0,0 +1,94 @@
+<?php
+class ControllerAccountTickets extends Controller {
+	public function index() {
+		if (!$this->customer->isLogged()) {
+			$this->session->data['redirect'] = $this->url->link('account/account');
+
+			$this->response->redirect($this->url->link('account/login'));
+		}
+
+		$this->load->language('account/account');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_account'),
+			'href' => $this->url->link('account/account')
+		);
+
+		if (isset($this->session->data['success'])) {
+			$data['success'] = $this->session->data['success'];
+
+			unset($this->session->data['success']);
+		} else {
+			$data['success'] = '';
+		} 
+		
+		$data['edit'] = $this->url->link('account/edit');
+		$data['password'] = $this->url->link('account/password');
+		$data['address'] = $this->url->link('account/address');
+		
+		$data['credit_cards'] = array();
+		
+		$files = glob(DIR_APPLICATION . 'controller/extension/credit_card/*.php');
+		
+		foreach ($files as $file) {
+			$code = basename($file, '.php');
+			
+			if ($this->config->get('payment_' . $code . '_status') && $this->config->get('payment_' . $code . '_card')) {
+				$this->load->language('extension/credit_card/' . $code, 'extension');
+
+				$data['credit_cards'][] = array(
+					'name' => $this->language->get('extension')->get('heading_title'),
+					'href' => $this->url->link('extension/credit_card/' . $code)
+				);
+			}
+		}
+		
+		$data['wishlist'] = $this->url->link('account/wishlist');
+		$data['order'] = $this->url->link('account/order');
+		$data['download'] = $this->url->link('account/download');
+		
+		if ($this->config->get('total_reward_status')) {
+			$data['reward'] = $this->url->link('account/reward');
+		} else {
+			$data['reward'] = '';
+		}		
+		
+		$data['return'] = $this->url->link('account/return');
+		$data['transaction'] = $this->url->link('account/transaction');
+		$data['newsletter'] = $this->url->link('account/newsletter');
+
+		$this->load->model('account/affiliate');
+		
+		$affiliate_info = $this->model_account_affiliate->getAffiliate($this->customer->getId());
+		
+		if (!$affiliate_info) {	
+			$data['affiliate'] = $this->url->link('account/affiliate/add');
+		} else {
+			$data['affiliate'] = $this->url->link('account/affiliate/edit');
+		}
+		
+		if ($affiliate_info) {		
+			$data['tracking'] = $this->url->link('account/tracking');
+		} else {
+			$data['tracking'] = '';
+		}
+		
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+		
+		$this->response->setOutput($this->load->view('account/tickets', $data));
+	}
+}
