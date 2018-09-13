ae471f1a1bd329086b192779e7d12923a06a7bb1
wangjunchao <378350151@qq.com>
2018-09-13 20:10:55 +0800

    wjc

--- catalog/controller/checkout/checkout.php
+++ catalog/controller/checkout/checkout.php
@@ -142,7 +142,7 @@ class ControllerCheckoutCheckout extends Controller {
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
-
+//var_dump($data['addresses']);
 		$this->response->setOutput($this->load->view('checkout/checkout/checkout', $data));
 	}
 
