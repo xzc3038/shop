ae59710a3d8e8e37f0890a7b8715e3f829b23ce7
wangjunchao <378350151@qq.com>
2018-09-14 17:14:08 +0800

    wjc

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -375,7 +375,7 @@ class ControllerProductCategory extends Controller {
 			$data['content_bottom'] = $this->load->controller('common/content_bottom');
 			$data['footer'] = $this->load->controller('common/footer');
 			$data['header'] = $this->load->controller('common/header');
-//var_dump( $data['column_left']);
+//var_dump( $data);
 			$this->response->setOutput($this->load->view('product/category', $data));
 		} else {
 			$url = '';
